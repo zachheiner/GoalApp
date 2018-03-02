b1fa90d506138c61a5aea126696200060d2cabd2
bin12011 <bin12011@byui.edu>
2018-03-02 15:27:48 -0700

    Added some javadocs documentation. We can change it later if needed.

--- app/src/main/java/com/example/zachheiner/goalapp/DisplayActivity.java
+++ app/src/main/java/com/example/zachheiner/goalapp/DisplayActivity.java
@@ -6,9 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+/**
+ * DisplayActivity
+ * Here our Second Activity receives the information from
+ * the main activity and is able to pull down the information
+ * needed by the user after their identity has been verified.
+ */
 public class DisplayActivity extends AppCompatActivity {
     private static final String TAG = "DisplayActivity";
 
+    /**
+     * onCreate
+     * we will create instances of the objects that the user
+     * has access to. In this case a listing of their current
+     * goals if they have been created or a listener waiting
+     * for the user to add a new goal for themselves.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
