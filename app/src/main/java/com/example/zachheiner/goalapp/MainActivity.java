1a927c582b246e9f27ce6dec73b4c07324a6fd70
MikeStarkJr <sta16001@byui.edu>
2018-03-07 14:15:39 -0700

    updating firebase connections and instances

--- app/src/main/java/com/example/zachheiner/goalapp/MainActivity.java
+++ app/src/main/java/com/example/zachheiner/goalapp/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import static com.example.zachheiner.goalapp.R.id.TextView_usernameField;
 import static com.example.zachheiner.goalapp.R.id.TextView_passwordField;
 
@@ -16,10 +21,24 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_USERNAME = "com.example.zachheiner.goalapp.EXTRA_USERNAME";
     public static final String EXTRA_PASSWORD = "com.example.zachheiner.goalapp.EXTRA_PASSWORD";
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mFirebaseDatabaseReference;
+    private DatabaseReference mUsersDatabaseReference;
+    private DatabaseReference mGoalsDatabaseReference;
+    private DatabaseReference mJournalDatabaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mUsersDatabaseReference = mFirebaseDatabase.getReference().child("user");
+        mGoalsDatabaseReference = mFirebaseDatabase.getReference().child("goal");
+        mJournalDatabaseReference = mFirebaseDatabase.getReference().child("journal");
 
         Button loginButton = (Button) findViewById(R.id.Button_login);
 
