69a33438d6659748fc0d6c94cac95f3c4bf5b89a
bin12011 <bin12011@byui.edu>
2018-03-09 09:52:49 -0700

    changed the User class to UserClass from DummyUserClass.

---
+++ app/src/main/java/com/example/zachheiner/goalapp/UserClass.java
@@ -0,0 +1,26 @@
+package com.example.zachheiner.goalapp;
+
+/**
+ * Created by dpbin on 3/9/2018.
+ */
+
+public class UserClass {
+    private String userName;
+    private String password;
+
+    public String getUser() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
