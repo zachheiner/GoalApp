b3c1d97728ae6caf8dfb1bdf1ef0b40af21957a3
zachattack <cardpunk1206@yahoo.com>
2018-04-02 18:20:54 -0600

    created the goalbars and text views. Also added the individual activity for goal view

---
+++ app/src/main/java/com/example/zachheiner/goalapp/GoalWindow.java
@@ -0,0 +1,13 @@
+package com.example.zachheiner.goalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoalWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_window);
+    }
+}
